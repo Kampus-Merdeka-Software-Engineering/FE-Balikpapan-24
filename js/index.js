5c679659eada730d739ce6521a12f2ea814aadc3
ivh02 <irmavernandini02@gmail.com>
2023-10-06 21:39:55 +0700

    add function fetch data

--- js/index.js
+++ js/index.js
@@ -1,24 +1,21 @@
-// const API_BASE_URL = 'https://railway-express-production-cb55.up.railway.app';
 const API_BASE_URL = 'http://localhost:3001';
 
 let lokasi = document.getElementById("lokasi");
 
-  // Perbaikan 1: Fungsi func_click sekarang menerima event sebagai parameter
   function func_click() {
     let user_input = document.getElementById("user_input").value;
 
-    // Perbaikan 2: Menggunakan fetch dengan alamat yang benar
-    fetch('http://localhost:3001/barangs')
+    fetch(`${API_BASE_URL}/barangs`)
       .then(response => response.json())
       .then(data => {
-        // Perbaikan 3: Menggunakan data.no_resi (nama properti yang benar)
+      
         for (let i = 0; i < data.length; i++) {
           if (user_input == data[i].no_resi) {
             lokasi.innerText = "Pesanan anda dengan Nomor Resi "+ data[i].no_resi+" sampai di = " + data[i].lokasi;
-            return; // Hentikan pencarian jika sudah ditemukan
+            return; 
           }
         }
-        // Perbaikan 4: Menambahkan pesan jika nomor resi tidak ditemukan
+
         lokasi.innerText = "Nomor resi tidak ditemukan";
       })
       .catch(error => {
@@ -26,27 +23,44 @@ let lokasi = document.getElementById("lokasi");
       });
   }
 
-//kode buatan sendiri sebelum dikoreksi chatgpt
-/* <input type="number" id="user_input" name="noresi" placeholder="Masukkan No Resi Anda" required><br><br> 
-<button onclick="func_click()" id="demo">SUBMIT</button>
-<p id="lokasi"></p>
-
-<script>
-  let lokasi =  document.getElementById("lokasi");
-  let user_input = document.getElementById("user_input").value;
-  func_click = () => {
-    fetch('http://localhost:3001/barangs')
-  .then(response => response.json())
-  .then(data => {
-      
-    let isi = JSON.stringify(data);
-      if (user_input === isi.noresi) {
-        lokasi.innerText = "Pesanan anda sampai di kota = "+isi.lokasi;
-      } 
-  .catch(error => {
-    lokasi.innerText = "Terjadi Kesalahan = "+ error;
-  })
-  }
-        }
+  async function fetchComments() {
+    try {
+      const response = await fetch(`${API_BASE_URL}/comments`);
+      const data = await response.json();
+      const commentList = document.getElementById('list-comment');
+      commentList.innerHTML = '';
   
-</script> */
+      data.forEach((comment) => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `${comment.username} : ${comment.isi_comment}\n`;
+        
+        commentList.appendChild(listItem);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  async function addNewComment() {
+    const username = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const isi_comment = document.getElementById('message').value;
+  
+    try {
+        const response = await fetch(`${API_BASE_URL}/comments`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, isi_comment })
+        });
+        const data = await response.json();
+    } catch (error) {
+        console.error('Error adding book:', error);
+    } finally {
+        fetchComments(); // Refresh the book list after adding a new book
+    }
+  }
+  
+  fetchComments();
+
--- js/nama_anggota.js
+++ js/nama_anggota.js
@@ -1,55 +1,109 @@
 const data = {   "data" : [
     {
         "id" : 1,
-        "nama" : "Defina Zul Faara"
+        "nama" : "Defina Zul Faara",
+        "role" : "Figma Designer & Frontend Engineer",
+        "prodi" : "Matematika"
     },
     {
         "id" : 2,
-        "nama" : "Fata Albana"
+        "nama" : "Fata Albana",
+        "role" : "Project Leader",
+        "prodi" : "Sistem Informasi"
     },
     {
         "id" : 3,
-        "nama" : "Laura Kristin"
+        "nama" : "Laura Kristin",
+        "role" : "Figma Designer & Backend Engineer",
+        "prodi" : "Ilmu Administrasi Publik"
     },
     {
         "id" : 4,
-        "nama" : "Irma Vernandini Hapsari"
+        "nama" : "Irma Vernandini Hapsari",
+        "role" : "Frontend Engineer & Backend Engineer",
+        "prodi" : "Statistika"
     },
     {
         "id" : 5,
-        "nama" : "Echa Pramudya"
+        "nama" : "Echa Pramudya",
+        "role" : "Frontend Engineer",
+        "prodi" : "Sistem Informasi",
     },
     {
         "id" : 6,
-        "nama" : "Tahlia Wahyu"
+        "nama" : "Tahlia Wahyu",
+        "role" : "Quality Assurance",
+        "prodi" : "Administrasi Negara",
     }
 ]
 }
 
-const anggota_1 = document.getElementById("anggota_1");
-const anggota_2 = document.getElementById("anggota_2");
-const anggota_3 = document.getElementById("anggota_3");
-const anggota_4 = document.getElementById("anggota_4");
-const anggota_5 = document.getElementById("anggota_5");
-const anggota_6 = document.getElementById("anggota_6");
+const output_name_1 = document.getElementById("nama_anggota_1");
+const output_role_1 = document.getElementById("role_anggota_1");
+const output_prodi_1 = document.getElementById("prodi_anggota_1");
+
+const output_name_2 = document.getElementById("nama_anggota_2");
+const output_role_2 = document.getElementById("role_anggota_2");
+const output_prodi_2 = document.getElementById("prodi_anggota_2");
+
+const output_name_3 = document.getElementById("nama_anggota_3");
+const output_role_3 = document.getElementById("role_anggota_3");
+const output_prodi_3 = document.getElementById("prodi_anggota_3");
+
+const output_name_4 = document.getElementById("nama_anggota_4");
+const output_role_4 = document.getElementById("role_anggota_4");
+const output_prodi_4 = document.getElementById("prodi_anggota_4");
+
+const output_name_5 = document.getElementById("nama_anggota_5");
+const output_role_5 = document.getElementById("role_anggota_5");
+const output_prodi_5 = document.getElementById("prodi_anggota_5");
+
+const output_name_6 = document.getElementById("nama_anggota_6");
+const output_role_6 = document.getElementById("role_anggota_6");
+const output_prodi_6 = document.getElementById("prodi_anggota_6");
 
 const nama_anggota_1 = data.data[0].nama;
-anggota_1.innerText = nama_anggota_1;
-        
+output_name_1.innerText = nama_anggota_1;
+const role_anggota_1 = data.data[0].role;
+output_role_1.innerText = role_anggota_1;
+const prodi_anggota_1 = data.data[0].prodi;
+output_prodi_1.innerText = prodi_anggota_1;
+
 const nama_anggota_2 = data.data[1].nama;
-anggota_2.innerText = nama_anggota_2;
+output_name_2.innerText = nama_anggota_2;
+const role_anggota_2 = data.data[1].role;
+output_role_2.innerText = role_anggota_2;
+const prodi_anggota_2 = data.data[1].prodi;
+output_prodi_2.innerText = prodi_anggota_2;
 
 const nama_anggota_3 = data.data[2].nama;
-anggota_3.innerText = nama_anggota_3;
+output_name_3.innerText = nama_anggota_3;
+const role_anggota_3 = data.data[2].role;
+output_role_3.innerText = role_anggota_3;
+const prodi_anggota_3 = data.data[2].prodi;
+output_prodi_3.innerText = prodi_anggota_3;
 
 const nama_anggota_4 = data.data[3].nama;
-anggota_4.innerText = nama_anggota_4;
+output_name_4.innerText = nama_anggota_4;
+const role_anggota_4 = data.data[3].role;
+output_role_4.innerText = role_anggota_4;
+const prodi_anggota_4 = data.data[3].prodi;
+output_prodi_4.innerText = prodi_anggota_4;
 
 const nama_anggota_5 = data.data[4].nama;
-anggota_5.innerText = nama_anggota_5;
+output_name_5.innerText = nama_anggota_5;
+const role_anggota_5 = data.data[4].role;
+output_role_5.innerText = role_anggota_5;
+const prodi_anggota_5 = data.data[4].prodi;
+output_prodi_5.innerText = prodi_anggota_5;
 
 const nama_anggota_6 = data.data[5].nama;
-anggota_6.innerText = nama_anggota_6;
+output_name_6.innerText = nama_anggota_6;
+const role_anggota_6 = data.data[5].role;
+output_role_6.innerText = role_anggota_6;
+const prodi_anggota_6 = data.data[5].prodi;
+output_prodi_6.innerText = prodi_anggota_6;
+
 
 
         
